5604a3939d06adbf6e33744af41c824a03cdae2a
JinDX <dox.jin@gmail.com>
2024-09-22 20:31:48 +0800

    checkcheck

--- watcher.js
+++ watcher.js
@@ -1,8 +1,8 @@
 const https = require('https');
 const window = require('./dropdown.js');
 
-const checkLinks = async () => {
-    const requests = window.links.map(link => {
+const checkLinks = async (links) => {
+    const requests = links.map(link => {
         return new Promise((resolve) => {
             https.get(link.src, { method: 'HEAD' }, (res) => {
                 const lastModified = res.headers['last-modified'];
@@ -20,29 +20,11 @@ const checkLinks = async () => {
     await Promise.all(requests);
 };
 
-const checkCssLinks = async () => {
-    const cssRequests = window.cssLinks.map(link => {
-        return new Promise((resolve) => {
-            https.get(link.src, { method: 'HEAD' }, (res) => {
-                const lastModified = res.headers['last-modified'];
-                if (lastModified && lastModified !== link["last-modified"]) {
-                    console.log(`${link.text}: Last Modified: ${lastModified} (Expected: ${link["last-modified"]})`);
-                }
-                resolve();
-            }).on('error', (e) => {
-                console.error(`Error fetching ${link.src}: ${e.message}`);
-                resolve();
-            });
-        });
-    });
-
-    await Promise.all(cssRequests);
-};
-
 const main = async () => {
-    await checkLinks();
-    await checkCssLinks();
-    process.exit(); // Exit the process after all requests are done
+    await checkLinks(window.links);
+    await checkLinks(window.cssLinks);
+    console.log('All checks completed.');
+    process.exit();
 };
 
 main();
