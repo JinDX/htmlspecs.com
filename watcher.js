3411453c7d8a87eb0b5169bb23a6e42d4ad5ffff
JinDX <dox.jin@gmail.com>
2025-05-24 11:11:25 +0800

    diffMin...

--- watcher.js
+++ watcher.js
@@ -3,14 +3,22 @@ const data = require('./data.js');
 
 const checkLinks = async (links) => {
     const requests = links.map(link => {
-        return new Promise((resolve) => {
-            https.get(link.src, { method: 'HEAD' }, (res) => {
+        return new Promise(resolve => {
+            https.get(link.src, { method: 'HEAD' }, res => {
                 const lastModified = res.headers['last-modified'];
-                if (lastModified && lastModified !== link["last-modified"]) {
-                    console.log(`${link.text}: Last Modified: ${lastModified} (Expected: ${link["last-modified"]})`);
+                if (lastModified) {
+                    const newTime = new Date(lastModified);
+                    const oldTime = new Date(link['last-modified']);
+                    const diffMs = Math.abs(newTime - oldTime);
+                    const diffMin = diffMs / 1000 / 60;
+                    if (diffMin >= 1) { // 差距到分鐘
+                        console.log(
+                            `${link.text}: new=${newTime.toISOString()} old=${oldTime.toISOString()} (Δ${diffMin.toFixed(1)}min)`
+                        );
+                    }
                 }
                 resolve();
-            }).on('error', (e) => {
+            }).on('error', e => {
                 console.error(`Error fetching ${link.src}: ${e.message}`);
                 resolve();
             });
