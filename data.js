19fc4cd5fe0ee30eb1bfa17b08689ac1d5d20239
JinDX <dox.jin@gmail.com>
2025-07-08 21:49:40 +0800

    dom Tue, 01 Jul 2025 16:52:51 GMT

--- data.js
+++ data.js
@@ -16,7 +16,7 @@ const links = [
         text: "DOM",
         href: "https://htmlspecs.com/dom",
         src: "https://dom.spec.whatwg.org/",
-        "last-modified": "Mon, 14 Apr 2025 15:51:50 GMT"
+        "last-modified": "Tue, 01 Jul 2025 16:52:51 GMT"
     },
     {
         text: "Web IDL",
