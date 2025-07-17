d4444a85c9d4771bcdf3dfb2d5363373237ae3e9
JinDX <dox.jin@gmail.com>
2025-07-17 11:01:31 +0800

    HTML last-modified

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const links = [
         text: "HTML",
         href: "https://htmlspecs.com/",
         src: "https://html.spec.whatwg.org/multipage/",
-        "last-modified": "Sat, 12 Jul 2025 06:39:53 GMT"
+        "last-modified": "Wed, 16 Jul 2025 01:00:46 GMT"
     },
     {
         text: "ECMA-262",
