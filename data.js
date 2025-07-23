8c76ce5447e0b4507da9a23a1d4fbd8974e46110
JinDX <dox.jin@gmail.com>
2025-07-23 10:03:43 +0800

    HTML 2025年7月22日

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const links = [
         text: "HTML",
         href: "https://htmlspecs.com/",
         src: "https://html.spec.whatwg.org/multipage/",
-        "last-modified": "Mon, 21 Jul 2025 09:11:02 GMT"
+        "last-modified": "Tue, 22 Jul 2025 03:42:32 GMT"
     },
     {
         text: "ECMA-262",
