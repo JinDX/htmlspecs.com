2741b8823b5f69f522fb429ec636f742be231cc0
JinDX <dox.jin@gmail.com>
2025-08-22 16:26:59 +0800

    intersection-observer jk

--- data.js
+++ data.js
@@ -453,7 +453,7 @@ const links = [
         href: "https://htmlspecs.com/intersection-observer",
         src: "https://www.w3.org/TR/2023/WD-intersection-observer-20231018/",
         "last-modified": "-",
-        lang: "c"
+        lang: "cjk"
     },
     {
         text: "Resize Observer",
