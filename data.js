962c9e81fad9c0b1855c7a2203ddb0d89319981a
JinDX <dox.jin@gmail.com>
2025-08-29 10:10:34 +0800

    resentation-api jk

--- data.js
+++ data.js
@@ -626,7 +626,7 @@ const links = [
         href: "https://htmlspecs.com/presentation-api",
         src: "https://www.w3.org/TR/2025/CRD-presentation-api-20250212/",
         "last-modified": "-",
-        lang: "c"
+        lang: "cjk"
     },
     {
         text: "Scroll-driven Animations",
