40ac9b404c8a0ff08725f9f770190470e984de4c
金东勋 | 김동훈 | 金東勳 | Jin Dongxun <dox.jin@gmail.com>
2025-09-02 10:03:40 +0800

    css-position-3 jk

--- data.js
+++ data.js
@@ -1040,7 +1040,7 @@ const cssLinks = [
         href: "https://htmlspecs.com/css/css-position-3",
         src: "https://www.w3.org/TR/2025/WD-css-position-3-20250311/",
         "last-modified": "-",
-        lang: "c"
+        lang: "cjk"
     },
 
     //Navigation
