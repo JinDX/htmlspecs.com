718cecbd306f3060475c8e4eababd1c3906e9860
JinDX <dox.jin@gmail.com>
2025-09-03 11:00:06 +0800

    css-writing-modes-4 jk

--- data.js
+++ data.js
@@ -1065,7 +1065,7 @@ const cssLinks = [
         href: "https://htmlspecs.com/css/css-writing-modes-4",
         src: "https://www.w3.org/TR/2019/CR-css-writing-modes-4-20190730/",
         "last-modified": "-",
-        lang: "c"
+        lang: "cjk"
     },
 
     //Fragmentation
