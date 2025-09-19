fe2cb3f97420eded757aa9217c946e352240bab3
JinDX <dox.jin@gmail.com>
2025-09-19 12:55:15 +0800

    ecma262 last-modified

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const links = [
         text: "ECMAScript",
         href: "https://ecma262.com/",
         src: "https://tc39.es/ecma262/",
-        "last-modified": "Tue, 16 Sep 2025 22:43:40 GMT",
+        "last-modified": "Fri, 19 Sep 2025 04:34:36 GMT",
         lang: "cjk",
         state: "Draft"
     },
