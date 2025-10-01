78ffb4389402b51a033eac8742de6a32f147408c
JinDX <dox.jin@gmail.com>
2025-10-01 15:08:25 +0800

    badging-20250930

--- data.js
+++ data.js
@@ -367,7 +367,7 @@ const links = [
     {
         text: "Badging API",
         href: "https://htmlspecs.com/badging",
-        src: "https://www.w3.org/TR/2025/WD-badging-20250925/",
+        src: "https://www.w3.org/TR/2025/WD-badging-20250930",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
