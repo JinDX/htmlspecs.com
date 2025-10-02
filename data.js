f45e343b3fee4688530fe1353763d62ab769c275
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2025-10-02 08:32:50 +0800

    sanitizer-api 2025-10-01

--- data.js
+++ data.js
@@ -604,7 +604,7 @@ const links = [
         text: "HTML Sanitizer API",
         href: "https://htmlspecs.com/sanitizer-api",
         src: "https://wicg.github.io/sanitizer-api/",
-        "last-modified": "Mon, 29 Sep 2025 12:31:24 GMT",
+        "last-modified": "Wed, 01 Oct 2025 16:19:07 GMT",
         lang: "cjk",
         state: "Draft"
     },
