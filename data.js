9001b7bfd21d716fbdecd0e8d9ac440ef4805536
JinDX <dox.jin@gmail.com>
2025-10-11 15:22:15 +0800

    title

--- data.js
+++ data.js
@@ -641,7 +641,7 @@ const links = [
         state: "WD"
     },
     {
-        text: "Permissions Policy Level 1",
+        text: "Permissions Policy",
         href: "https://htmlspecs.com/permissions-policy-1",
         src: "https://www.w3.org/TR/2025/WD-permissions-policy-1-20251006/",
         "last-modified": "-",
