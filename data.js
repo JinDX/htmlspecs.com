0a5a764aac6d5498f1515bb76836f2a0197cfec3
JinDX <dox.jin@gmail.com>
2025-10-17 19:24:24 +0800

    sanitizer-api 2025-10-17

--- data.js
+++ data.js
@@ -684,7 +684,7 @@ const links = [
         text: "HTML Sanitizer API",
         href: "https://htmlspecs.com/sanitizer-api",
         src: "https://wicg.github.io/sanitizer-api/",
-        "last-modified": "Wed, 15 Oct 2025 09:25:24 GMT",
+        "last-modified": "Fri, 17 Oct 2025 09:32:29 GMT",
         lang: "cjk",
         state: "Draft"
     },
