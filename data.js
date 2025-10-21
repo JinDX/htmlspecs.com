6bad993f38fe3f3be292c612b1fa5931bf203dd0
JinDX <dox.jin@gmail.com>
2025-10-21 12:40:24 +0800

    design-principles-20251021

--- data.js
+++ data.js
@@ -111,7 +111,7 @@ const links = [
     {
         text: "Web Platform Design Principles",
         href: "https://htmlspecs.com/design-principles",
-        src: "https://www.w3.org/TR/2025/NOTE-design-principles-20251020/",
+        src: "https://www.w3.org/TR/2025/NOTE-design-principles-20251021/",
         "last-modified": "-",
         lang: "cjk",
         state: "NOTE"
