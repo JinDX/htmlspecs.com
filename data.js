00c79f02eeade24eb83e78d75ae1258559eb7882
JinDX <dox.jin@gmail.com>
2025-10-21 10:36:28 +0800

    WGSL-20251020

--- data.js
+++ data.js
@@ -782,7 +782,7 @@ const links = [
     {
         text: "WebGPU Shading Language",
         href: "https://htmlspecs.com/WGSL",
-        src: "https://www.w3.org/TR/2025/CRD-WGSL-20251017/",
+        src: "https://www.w3.org/TR/2025/CRD-WGSL-20251020/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
