979ddfec95f6fecfa879feb4bc86ad9c3f713e98
JinDX <dox.jin@gmail.com>
2025-10-22 14:56:09 +0800

    webgpu-20251022

--- data.js
+++ data.js
@@ -774,7 +774,7 @@ const links = [
     {
         text: "WebGPU",
         href: "https://htmlspecs.com/webgpu",
-        src: "https://www.w3.org/TR/2025/CRD-webgpu-20251014/",
+        src: "https://www.w3.org/TR/2025/CRD-webgpu-20251022/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
