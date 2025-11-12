d16a9aa7262a71ff7531a47d3e37d53045b2f938
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2025-11-12 08:33:10 +0800

    min-common-api 2025-11-11

--- data.js
+++ data.js
@@ -168,7 +168,7 @@ const links = [
         text: "Minimum common web API",
         href: "https://htmlspecs.com/min-common-api",
         src: "https://min-common-api.proposal.wintertc.org/",
-        "last-modified": "Tue, 11 Nov 2025 03:12:32 GMT",
+        "last-modified": "Tue, 11 Nov 2025 14:45:01 GMT",
         lang: "cjk",
         state: "Draft"
     },
