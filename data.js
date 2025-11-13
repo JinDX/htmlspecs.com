0585e056e3ab31bf6dd60be9290766e3b56f3e2c
JinDX <dox.jin@gmail.com>
2025-11-13 18:13:22 +0800

    HTML 251113

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const links = [
         text: "HTML",
         href: "https://htmlspecs.com/",
         src: "https://html.spec.whatwg.org/multipage/",
-        "last-modified": "Tue, 04 Nov 2025 10:59:30 GMT",
+        "last-modified": "Thu, 13 Nov 2025 08:49:29 GMT",
         lang: "cjk",
         state: "LS"
     },
