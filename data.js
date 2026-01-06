5ce3466307cf1877897296683ffad8bfa045b4ca
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-01-06 13:29:30 +0800

    WGSL-20260106

--- data.js
+++ data.js
@@ -1310,7 +1310,7 @@ const links = [
     {
         text: "WebGPU Shading Language",
         href: "https://htmlspecs.com/WGSL/",
-        src: "https://www.w3.org/TR/2025/CRD-WGSL-20251215/",
+        src: "https://www.w3.org/TR/2026/CRD-WGSL-20260106/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
