755161ce3fc1bddf077ce20578465af15a40832f
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-01-23 09:19:55 +0800

    mathml4-20260122

--- data.js
+++ data.js
@@ -246,7 +246,7 @@ const links = [
     {
         text: "MathML4",
         href: "https://htmlspecs.com/mathml4/",
-        src: "https://www.w3.org/TR/2025/WD-mathml4-20251218/",
+        src: "https://www.w3.org/TR/2026/WD-mathml4-20260122/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
