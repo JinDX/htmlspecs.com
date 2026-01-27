ed9bad4b53f5143706854857c00a2747adaff365
JinDX <dox.jin@gmail.com>
2026-01-27 15:41:30 +0800

    payment-request-20260127

--- data.js
+++ data.js
@@ -443,7 +443,7 @@ const links = [
     {
         text: "Payment Request API",
         href: "https://htmlspecs.com/payment-request/",
-        src: "https://www.w3.org/TR/2025/CRD-payment-request-20250930/",
+        src: "https://www.w3.org/TR/2026/CRD-payment-request-20260127/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
