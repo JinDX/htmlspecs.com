70ef0876bd7338d37cc8c7235414318455ec48f9
JinDX <dox.jin@gmail.com>
2026-01-29 16:35:42 +0800

    webtransport-20260129

--- data.js
+++ data.js
@@ -411,7 +411,7 @@ const links = [
     {
         text: "WebTransport",
         href: "https://htmlspecs.com/webtransport/",
-        src: "https://www.w3.org/TR/2025/WD-webtransport-20251217/",
+        src: "https://www.w3.org/TR/2026/WD-webtransport-20260129/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
