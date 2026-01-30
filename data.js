8ebb26b433f1191411e081faff5b86f2bdc0b72a
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-01-30 09:04:27 +0800

    appmanifest-20260129

--- data.js
+++ data.js
@@ -174,7 +174,7 @@ const links = [
     {
         text: "Web Application Manifest",
         href: "https://htmlspecs.com/appmanifest/",
-        src: "https://www.w3.org/TR/2026/WD-appmanifest-20260105/",
+        src: "https://www.w3.org/TR/2026/WD-appmanifest-20260129/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
