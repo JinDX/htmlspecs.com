72bda1a73612eaf4166a833f722e84666a017007
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-02-09 08:58:25 +0800

    HTML Sun, 08 Feb 2026 15:44:40 GMT

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const links = [
         text: "HTML",
         href: "https://htmlspecs.com/",
         src: "https://html.spec.whatwg.org/multipage/",
-        "last-modified": "Thu, 05 Feb 2026 08:22:07 GMT",
+        "last-modified": "Sun, 08 Feb 2026 15:44:40 GMT",
         lang: "cjk",
         state: "LS"
     },
