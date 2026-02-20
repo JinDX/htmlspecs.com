17333e47f98f6547e9c2b29a89d4561e9c72e340
JinDX <dox.jin@gmail.com>
2026-02-20 14:17:58 +0800

    service-workers-20260220

--- data.js
+++ data.js
@@ -419,7 +419,7 @@ const links = [
     {
         text: "Service Workers Nightly",
         href: "https://htmlspecs.com/service-workers/",
-        src: "https://www.w3.org/TR/2026/CRD-service-workers-20260126/",
+        src: "https://www.w3.org/TR/2026/CRD-service-workers-20260220/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
