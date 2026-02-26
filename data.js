bfb254934a4b57de0faae0550a9dcef05b46abe4
JinDX <dox.jin@gmail.com>
2026-02-26 19:44:32 +0800

    wasm-js-api-2-20260226

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const links = [
     {
         text: "WebAssembly JavaScript Interface",
         href: "https://htmlspecs.com/wasm-js-api-2/",
-        src: "https://www.w3.org/TR/2026/CRD-wasm-js-api-2-20260224/",
+        src: "https://www.w3.org/TR/2026/CRD-wasm-js-api-2-20260226/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
