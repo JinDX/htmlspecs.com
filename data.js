289379a56d8ca15317c39fbf851addc91a878340
JinDX <dox.jin@gmail.com>
2026-02-26 20:06:39 +0800

    picture-in-picture-20260226

--- data.js
+++ data.js
@@ -1408,7 +1408,7 @@ const links = [
     {
         text: "Picture-in-Picture",
         href: "https://htmlspecs.com/picture-in-picture/",
-        src: "https://www.w3.org/TR/2026/WD-picture-in-picture-20260130/",
+        src: "https://www.w3.org/TR/2026/WD-picture-in-picture-20260226/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
