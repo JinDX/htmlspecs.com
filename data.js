1487a0c00c2304d32deffd0b01674967b719f839
JinDX <dox.jin@gmail.com>
2026-03-15 20:09:06 +0800

    fetch Sun, 15 Mar 2026 09:02:47 GMT

--- data.js
+++ data.js
@@ -324,7 +324,7 @@ const links = [
         text: "Fetch",
         href: "https://htmlspecs.com/fetch/",
         src: "https://fetch.spec.whatwg.org/",
-        "last-modified": "Wed, 28 Jan 2026 13:14:04 GMT",
+        "last-modified": "Sun, 15 Mar 2026 09:02:47 GMT",
         lang: "cjk",
         state: "LS"
     },
