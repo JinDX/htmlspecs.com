f9ed1584ce7f94b0660ae9f922738691e8d651af
JinDX <dox.jin@gmail.com>
2026-03-26 08:12:46 +0800

    ecma262 Wed, 25 Mar 2026 17:22:18 GMT

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const links = [
         text: "ECMAScript® 2026 Language Specification",
         href: "https://ecma262.com/",
         src: "https://tc39.es/ecma262/",
-        "last-modified": "Tue, 24 Mar 2026 15:24:04 GMT",
+        "last-modified": "Wed, 25 Mar 2026 17:22:18 GMT",
         lang: "cjk",
         state: "Draft"
     },
