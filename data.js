6963fda1cb97769b5d61a5f1fae8f06baa9bab51
JinDX <dox.jin@gmail.com>
2026-04-17 11:28:37 +0800

    ecma402 Fri, 17 Apr 2026 00:27:33 GMT

--- data.js
+++ data.js
@@ -31,7 +31,7 @@ const links = [
         text: "ECMAScript® 2027 Internationalization API Specification",
         href: "https://htmlspecs.com/ecma402/",
         src: "https://tc39.es/ecma402/",
-        "last-modified": "Tue, 07 Apr 2026 10:54:43 GMT",
+        "last-modified": "Fri, 17 Apr 2026 00:27:33 GMT",
         lang: "cjk",
         state: "Draft"
     },
