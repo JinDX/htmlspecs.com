ec40214a2329c2dd5ab29df544bc8e3f6ddb96b9
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-04-20 09:47:06 +0800

    CSP3-20260419

--- data.js
+++ data.js
@@ -1229,7 +1229,7 @@ const links = [
     {
         text: "Content Security Policy Level 3",
         href: "https://htmlspecs.com/CSP3/",
-        src: "https://www.w3.org/TR/2026/WD-CSP3-20260401/",
+        src: "https://www.w3.org/TR/2026/WD-CSP3-20260419/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
