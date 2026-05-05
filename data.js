2e88e6d4403f10cb821ae1fb008121849390563c
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-05-05 09:45:03 +0800

    webcodecs-20260505

--- data.js
+++ data.js
@@ -1512,7 +1512,7 @@ const links = [
     {
         text: "WebCodecs",
         href: "https://htmlspecs.com/webcodecs/",
-        src: "https://www.w3.org/TR/2026/WD-webcodecs-20260414/",
+        src: "https://www.w3.org/TR/2026/WD-webcodecs-20260505/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
