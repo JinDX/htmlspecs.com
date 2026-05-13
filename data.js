5e3e931b55e0d3760ad9315cd831d8a3ecf3d5d6
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-05-13 10:02:58 +0800

    webgpu-20260512

--- data.js
+++ data.js
@@ -1520,7 +1520,7 @@ const links = [
     {
         text: "WebGPU",
         href: "https://htmlspecs.com/webgpu/",
-        src: "https://www.w3.org/TR/2026/CRD-webgpu-20260507/",
+        src: "https://www.w3.org/TR/2026/CRD-webgpu-20260512/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
