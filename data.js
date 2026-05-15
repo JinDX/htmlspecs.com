4af54bcd1925747ea65733d9f5a085d8684ebcd7
JinDX <dox.jin@gmail.com>
2026-05-15 09:31:53 +0800

    HTML Thu, 14 May 2026 18:08:26 GMT

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const links = [
         text: "HTML",
         href: "https://htmlspecs.com/",
         src: "https://html.spec.whatwg.org/multipage/",
-        "last-modified": "Thu, 14 May 2026 12:58:49 GMT",
+        "last-modified": "Thu, 14 May 2026 18:08:26 GMT",
         lang: "cjk",
         state: "LS"
     },
