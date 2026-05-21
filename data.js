2b56e8ebb4c09f1600acb6d86f3d1b87192d019a
JinDX <dox.jin@gmail.com>
2026-05-21 14:36:32 +0800

    attribution-20260521

--- data.js
+++ data.js
@@ -1565,7 +1565,7 @@ const links = [
     {
         text: "Attribution Level 1",
         href: "https://htmlspecs.com/attribution/",
-        src: "https://www.w3.org/TR/2026/WD-attribution-20260514/",
+        src: "https://www.w3.org/TR/2026/WD-attribution-20260521/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
