9dc534ff37199eadef76ecda86f27985fffa2779
JinDX <dox.jin@gmail.com>
2026-06-05 12:30:15 +0800

    core-aam-1.2-20260604

--- data.js
+++ data.js
@@ -336,7 +336,7 @@ const links = [
     {
         text: "Core Accessibility API Mappings 1.2",
         href: "https://htmlspecs.com/core-aam-1.2/",
-        src: "https://www.w3.org/TR/2026/CRD-core-aam-1.2-20260529/",
+        src: "https://www.w3.org/TR/2026/CRD-core-aam-1.2-20260604/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
