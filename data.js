bc256c4e6b158cf03d425db828a87f699b3c8ff3
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-06-09 09:06:02 +0800

    service-workers-20260608

--- data.js
+++ data.js
@@ -523,7 +523,7 @@ const links = [
     {
         text: "Service Workers Nightly",
         href: "https://htmlspecs.com/service-workers/",
-        src: "https://www.w3.org/TR/2026/CRD-service-workers-20260526/",
+        src: "https://www.w3.org/TR/2026/CRD-service-workers-20260608/",
         "last-modified": "-",
         lang: "cjk",
         state: "CRD"
