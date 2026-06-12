534dc7300c11bc4c6100a2ca408b93bf41253a23
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-06-12 09:30:24 +0800

    selection-api-20260611

--- data.js
+++ data.js
@@ -1160,7 +1160,7 @@ const links = [
     {
         text: "Selection API",
         href: "https://htmlspecs.com/selection-api/",
-        src: "https://www.w3.org/TR/2026/WD-selection-api-20260605/",
+        src: "https://www.w3.org/TR/2026/WD-selection-api-20260611/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
