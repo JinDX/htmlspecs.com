af9f3da4dc92d057aa2e99982fe9dca94468dc12
JinDX <dox.jin@gmail.com>
2026-06-12 20:16:18 +0800

    HTML Fri, 12 Jun 2026 10:56:02 GMT

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const links = [
         text: "HTML",
         href: "https://htmlspecs.com/",
         src: "https://html.spec.whatwg.org/",
-        "last-modified": "Wed, 10 Jun 2026 08:46:47 GMT",
+        "last-modified": "Fri, 12 Jun 2026 10:56:02 GMT",
         lang: "cjk",
         state: "LS"
     },
