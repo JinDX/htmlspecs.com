6b1b32c3d5c4d6bfd4077f84e612c05bbf89fccc
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-06-18 10:07:12 +0800

    dapt

--- data.js
+++ data.js
@@ -1389,14 +1389,14 @@ const links = [
         lang: "cjk",
         state: "REC"
     },
-    // {
-    //     text: "Dubbing and Audio description Profiles of TTML2",
-    //     href: "https://htmlspecs.com/dapt/",
-    //     src: "https://www.w3.org/TR/2026/CRD-dapt-20260210/",
-    //     "last-modified": "-",
-    //     lang: "cjk",
-    //     state: "CRD"
-    // },
+    {
+        text: "Dubbing and Audio description Profiles of TTML2",
+        href: "https://htmlspecs.com/dapt/",
+        src: "https://www.w3.org/TR/2026/CRD-dapt-20260210/",
+        "last-modified": "-",
+        lang: "cjk",
+        state: "CRD"
+    },
     {
         text: "Media Source Extensions",
         href: "https://htmlspecs.com/media-source-2/",
