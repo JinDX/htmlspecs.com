a12ad579eff57d66cf8dd6599dc48d402c02b2e3
JinDX <dox.jin@gmail.com>
2026-06-20 15:59:14 +0800

    ............

--- data.js
+++ data.js
@@ -3884,7 +3884,7 @@ const httpLinks = [
         href: "https://htmlspecs.com/http/no-vary-search.html",
         src: "https://httpwg.org/http-extensions/draft-ietf-httpbis-no-vary-search.html",
         "last-modified": "-",
-        etag: "6a361926-1b5d3",
+        etag: "6a361927-1b5d3",
         lang: "cjk",
         state: "Draft"
     },
