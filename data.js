f94d172f76802e86f345dade1eaad7a631e1ddc9
JinDX <dox.jin@gmail.com>
2026-07-09 19:03:15 +0800

    digital-credentials-20260709

--- data.js
+++ data.js
@@ -2765,7 +2765,7 @@ const links = [
     {
         text: "Digital Credentials",
         href: "https://htmlspecs.com/digital-credentials/",
-        src: "https://www.w3.org/TR/2026/WD-digital-credentials-20260626/",
+        src: "https://www.w3.org/TR/2026/WD-digital-credentials-20260709/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
