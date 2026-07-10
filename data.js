4121423f7050b1a733f39733be2a94e5db74ac62
JinDX <dox.jin@gmail.com>
2026-07-10 19:12:39 +0800

    did-resolution-20260710

--- data.js
+++ data.js
@@ -2781,7 +2781,7 @@ const links = [
     {
         text: "Decentralized Identifier Resolution (DID Resolution) v0.3",
         href: "https://htmlspecs.com/did-resolution/",
-        src: "https://www.w3.org/TR/2026/WD-did-resolution-20260709/",
+        src: "https://www.w3.org/TR/2026/WD-did-resolution-20260710/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
