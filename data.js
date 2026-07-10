434d49e552fdf7958fd202438c3e6ef794fec401
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-07-10 10:19:52 +0800

    did-resolution-20260709

--- data.js
+++ data.js
@@ -2781,7 +2781,7 @@ const links = [
     {
         text: "Decentralized Identifier Resolution (DID Resolution) v0.3",
         href: "https://htmlspecs.com/did-resolution/",
-        src: "https://www.w3.org/TR/2026/WD-did-resolution-20260611/",
+        src: "https://www.w3.org/TR/2026/WD-did-resolution-20260709/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
