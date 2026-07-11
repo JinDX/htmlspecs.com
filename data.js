d997ed4bfa59e5979e1ea590195c43670c403090
JinDX <dox.jin@gmail.com>
2026-07-11 08:36:51 +0800

    ecma262 last-modified

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const links = [
         text: "ECMAScript® 2027 Language Specification",
         href: "https://ecma262.com/",
         src: "https://tc39.es/ecma262/",
-        "last-modified": "Fri, 10 Jul 2026 00:22:50 GMT",
+        "last-modified": "Fri, 10 Jul 2026 22:28:40 GMT",
         lang: "cjk",
         state: "Draft"
     },
