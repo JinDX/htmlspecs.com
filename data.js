a8aa672e62a7c63ba2b8d2f514615b889f4135f7
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-07-17 09:49:16 +0800

    ecma262 last-modified

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const links = [
         text: "ECMAScript® 2027 Language Specification",
         href: "https://ecma262.com/",
         src: "https://tc39.es/ecma262/",
-        "last-modified": "Tue, 14 Jul 2026 15:45:58 GMT",
+        "last-modified": "Thu, 16 Jul 2026 23:42:25 GMT",
         lang: "cjk",
         state: "Draft"
     },
