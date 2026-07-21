bfd5cf3f283aed5be3508210315198535fafb4f8
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-07-21 13:36:00 +0800

    shacl12-core-20260720

--- data.js
+++ data.js
@@ -612,7 +612,7 @@ const links = [
     {
         text: "SHACL 1.2 Core",
         href: "https://htmlspecs.com/shacl12-core/",
-        src: "https://www.w3.org/TR/2026/WD-shacl12-core-20260630/",
+        src: "https://www.w3.org/TR/2026/WD-shacl12-core-20260720/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
