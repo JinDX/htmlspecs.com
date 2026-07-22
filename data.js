8638ac310cf07ae765298926257ed03d47a1022a
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-07-22 14:04:18 +0800

    vcalm-1.0-20260721

--- data.js
+++ data.js
@@ -2829,7 +2829,7 @@ const links = [
     {
         text: "VCALM v1.0",
         href: "https://htmlspecs.com/vcalm-1.0/",
-        src: "https://www.w3.org/TR/2026/WD-vcalm-1.0-20260714/",
+        src: "https://www.w3.org/TR/2026/WD-vcalm-1.0-20260721/",
         "last-modified": "-",
         lang: "cjk",
         state: "WD"
