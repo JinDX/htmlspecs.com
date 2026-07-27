244a9adbd8f4c2f68f861bf2f2fd8f1cd28ff95d
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-07-27 16:53:50 +0800

    wasm-js-api-2-20260727

--- data.js
+++ data.js
@@ -90,7 +90,7 @@ const linkGroups = [
             {
                 text: "WebAssembly JavaScript Interface",
                 href: "https://htmlspecs.com/wasm-js-api-2/",
-                src: "https://www.w3.org/TR/2026/CRD-wasm-js-api-2-20260724/",
+                src: "https://www.w3.org/TR/2026/CRD-wasm-js-api-2-20260727/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "CRD"
