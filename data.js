f8677053fa1b1262272413d0dc197d24854d7d2b
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-07-29 09:52:21 +0800

    web-sustainability-guidelines-20260728

--- data.js
+++ data.js
@@ -194,7 +194,7 @@ const linkGroups = [
             {
                 text: "Web Sustainability Guidelines (WSG)",
                 href: "https://htmlspecs.com/web-sustainability-guidelines/",
-                src: "https://www.w3.org/TR/2026/DNOTE-web-sustainability-guidelines-20260724/",
+                src: "https://www.w3.org/TR/2026/DNOTE-web-sustainability-guidelines-20260728/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "DNOTE"
