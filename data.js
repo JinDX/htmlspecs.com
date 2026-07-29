a5f8c08661d33e8cb43ba01a28cb220168b498be
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-07-29 10:33:38 +0800

    attribution-20260729

--- data.js
+++ data.js
@@ -3323,7 +3323,7 @@ const linkGroups = [
             {
                 text: "Attribution Level 1",
                 href: "https://htmlspecs.com/attribution/",
-                src: "https://www.w3.org/TR/2026/WD-attribution-20260727/",
+                src: "https://www.w3.org/TR/2026/WD-attribution-20260729/,
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
