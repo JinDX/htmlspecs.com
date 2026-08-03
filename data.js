e59f061596df1e65de9d6d2ee0d25c0e68e855c8
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-08-03 17:06:34 +0800

    no-vary-search 2026-08-03

--- data.js
+++ data.js
@@ -5089,7 +5089,7 @@ const httpLinkGroups = [
                 href: "https://htmlspecs.com/http/no-vary-search.html",
                 src: "https://httpwg.org/http-extensions/draft-ietf-httpbis-no-vary-search.html",
                 "last-modified": "-",
-                etag: "6a6c9e7e-1c896",
+                etag: "6a701870-1cfa6",
                 lang: "cjk",
                 state: "Draft"
             },
