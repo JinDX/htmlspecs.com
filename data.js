1010e1e0b13057b4130a048535abfc9d93ae9caf
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-08-04 09:14:01 +0800

    container-timing Mon, 03 Aug 2026 17:24:54 GMT

--- data.js
+++ data.js
@@ -2616,7 +2616,7 @@ const linkGroups = [
                 text: "Container Timing API",
                 href: "https://htmlspecs.com/container-timing/",
                 src: "https://wicg.github.io/container-timing/",
-                "last-modified": "Wed, 29 Jul 2026 13:48:46 GMT",
+                "last-modified": "Mon, 03 Aug 2026 17:24:54 GMT",
                 lang: "cjk",
                 state: "Draft"
             },
