4b0cf277fae2cee9030dbdf4359ee7601fb3a457
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-08-12 16:00:07 +0800

    css-fonts-5-20260811

--- data.js
+++ data.js
@@ -4037,7 +4037,7 @@ const cssLinkGroups = [
             {
                 text: "CSS Fonts Module Level 5",
                 href: "https://htmlspecs.com/css/css-fonts-5/",
-                src: "https://www.w3.org/TR/2026/WD-css-fonts-5-20260303/",
+                src: "https://www.w3.org/TR/2026/WD-css-fonts-5-20260811/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
