3462582fa8b092be500f357886e8ae76658b3aa5
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-08-13 08:47:35 +0800

    cross-origin-storage Wed, 12 Aug 2026 16:35:03 GMT

--- data.js
+++ data.js
@@ -1241,7 +1241,7 @@ const linkGroups = [
                 text: "Cross-Origin Storage",
                 href: "https://htmlspecs.com/cross-origin-storage/",
                 src: "https://wicg.github.io/cross-origin-storage/",
-                "last-modified": "Wed, 12 Aug 2026 09:31:15 GMT",
+                "last-modified": "Wed, 12 Aug 2026 16:35:03 GMT",
                 lang: "cjk",
                 state: "Draft"
             },
