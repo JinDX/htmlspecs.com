6f41fe91c50c891e9d5ff51f1a94e5277b52ac31
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-08-27 08:35:43 +0800

    html Wed, 26 Aug 2026 18:52:16 GMT

--- data.js
+++ data.js
@@ -35,7 +35,7 @@ const linkGroups = [
                 text: "HTML",
                 href: "https://htmlspecs.com/",
                 src: "https://html.spec.whatwg.org/",
-                "last-modified": "Wed, 26 Aug 2026 08:41:22 GMT",
+                "last-modified": "Wed, 26 Aug 2026 18:52:16 GMT",
                 lang: "cjk",
                 state: "LS"
             },
