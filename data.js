1385058f62a7cb5044cda2af394727d4d156748e
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-01 14:45:26 +0800

    webdriver-bidi-20260901

--- data.js
+++ data.js
@@ -3694,7 +3694,7 @@ const linkGroups = [
             {
                 text: "WebDriver BiDi",
                 href: "https://htmlspecs.com/webdriver-bidi/",
-                src: "https://www.w3.org/TR/2026/WD-webdriver-bidi-20260825/",
+                src: "https://www.w3.org/TR/2026/WD-webdriver-bidi-20260901/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
