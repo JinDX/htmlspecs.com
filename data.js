27f944553734c806a7244514dd37c5ed2e2e2139
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-03 09:07:52 +0800

    webmcp Wed, 02 Sep 2026 14:09:04 GMT

--- data.js
+++ data.js
@@ -2401,7 +2401,7 @@ const linkGroups = [
                 text: "WebMCP",
                 href: "https://htmlspecs.com/webmcp/",
                 src: "https://webmachinelearning.github.io/webmcp/",
-                "last-modified": "Wed, 26 Aug 2026 20:24:00 GMT",
+                "last-modified": "Wed, 02 Sep 2026 14:09:04 GMT",
                 lang: "cjk",
                 state: "Draft"
             },
