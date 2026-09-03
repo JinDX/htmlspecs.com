1863629c5b82e846f4f1d2ee40431501705acc46
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-03 19:37:17 +0800

    digital-credentials-20260903

--- data.js
+++ data.js
@@ -3012,7 +3012,7 @@ const linkGroups = [
             {
                 text: "Digital Credentials",
                 href: "https://htmlspecs.com/digital-credentials/",
-                src: "https://www.w3.org/TR/2026/WD-digital-credentials-20260827/",
+                src: "https://www.w3.org/TR/2026/WD-digital-credentials-20260903/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
