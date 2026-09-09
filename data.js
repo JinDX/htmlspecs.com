88b088c7613fdc897f434e4aedb1be0b1c7730c9
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-09 09:16:45 +0800

    web-user-agents-20260908

--- data.js
+++ data.js
@@ -170,7 +170,7 @@ const linkGroups = [
             {
                 text: "Web User Agents",
                 href: "https://htmlspecs.com/web-user-agents/",
-                src: "https://www.w3.org/TR/2026/DNOTE-web-user-agents-20260907/",
+                src: "https://www.w3.org/TR/2026/DNOTE-web-user-agents-20260908/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "DNOTE"
