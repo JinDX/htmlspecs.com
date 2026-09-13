4eedc5f7cfdce9b8ba5063760625290d0e1edb4b
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-13 09:23:08 +0800

    epub-a11y-12-20260912

--- data.js
+++ data.js
@@ -3663,7 +3663,7 @@ const linkGroups = [
             {
                 text: "EPUB Accessibility 1.2",
                 href: "https://htmlspecs.com/epub-a11y-12/",
-                src: "https://www.w3.org/TR/2026/CRD-epub-a11y-12-20260818/",
+                src: "https://www.w3.org/TR/2026/CRD-epub-a11y-12-20260912/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "CRD"
