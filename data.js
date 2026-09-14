d9ca45a8234a7d2089c40024f521d826be89275c
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-14 09:57:21 +0800

    css-color-5-20260913

--- data.js
+++ data.js
@@ -3966,7 +3966,7 @@ const cssLinkGroups = [
             {
                 text: "CSS Color Module Level 5",
                 href: "https://htmlspecs.com/css/css-color-5/",
-                src: "https://www.w3.org/TR/2026/WD-css-color-5-20260908/",
+                src: "https://www.w3.org/TR/2026/WD-css-color-5-20260913/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
