9d4960c9cc89a4b4593f17d2ccc4c82b3299ee12
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-15 08:58:51 +0800

    design-principles-20260914

--- data.js
+++ data.js
@@ -186,7 +186,7 @@ const linkGroups = [
             {
                 text: "Web Platform Design Principles",
                 href: "https://htmlspecs.com/design-principles/",
-                src: "https://www.w3.org/TR/2026/NOTE-design-principles-20260911/",
+                src: "https://www.w3.org/TR/2026/NOTE-design-principles-20260914/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "NOTE"
