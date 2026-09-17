bb03293cbb392036036a8fa8b78af905544ca624
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-17 08:41:57 +0800

    cbor-ld-10-20260916

--- data.js
+++ data.js
@@ -391,7 +391,7 @@ const linkGroups = [
             {
                 text: "CBOR-LD 1.0",
                 href: "https://htmlspecs.com/cbor-ld-10/",
-                src: "https://www.w3.org/TR/2026/WD-cbor-ld-10-20260902/",
+                src: "https://www.w3.org/TR/2026/WD-cbor-ld-10-20260916/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
