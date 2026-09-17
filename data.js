c0f091cb77e5f1c42633e530f6e7417a7548b15a
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-18 07:10:18 +0800

    openapi last-modified

--- data.js
+++ data.js
@@ -736,7 +736,7 @@ const linkGroups = [
                 text: "OpenAPI Specification v3.2.1",
                 href: "https://htmlspecs.com/openapi/",
                 src: "https://spec.openapis.org/oas/latest.html",
-                "last-modified": "Thu, 17 Sep 2026 09:45:46 GMT",
+                "last-modified": "Thu, 17 Sep 2026 21:25:26 GMT",
                 lang: "cjk",
                 state: "Draft"
             },
