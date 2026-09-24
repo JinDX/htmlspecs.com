97e0d08bffdd6b97a63b38143f4c9fa28f48435b
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-24 10:33:14 +0800

    accname-1.2-20260923

--- data.js
+++ data.js
@@ -3608,7 +3608,7 @@ const linkGroups = [
             {
                 text: "Accessible Name and Description Computation 1.2",
                 href: "https://htmlspecs.com/accname-1.2/",
-                src: "https://www.w3.org/TR/2026/WD-accname-1.2-20260922/",
+                src: "https://www.w3.org/TR/2026/WD-accname-1.2-20260923/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
