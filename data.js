1f160592498fe2bc3f2c483fd37b89897510da4f
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-25 15:10:40 +0800

    svg-aam-1.0-20260924

--- data.js
+++ data.js
@@ -3560,7 +3560,7 @@ const linkGroups = [
             {
                 text: "SVG Accessibility API Mappings",
                 href: "https://htmlspecs.com/svg-aam-1.0/",
-                src: "https://www.w3.org/TR/2026/WD-svg-aam-1.0-20260923/",
+                src: "https://www.w3.org/TR/2026/WD-svg-aam-1.0-20260924/",
                 "last-modified": "-",
                 lang: "cjk",
                 state: "WD"
