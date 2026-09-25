fad22944b21ac9dd92b14ddc38b693bea8a74444
金东勋 | 金東勳 | 김동훈 | Jin Dongxun <dox.jin@gmail.com>
2026-09-25 08:35:35 +0800

    ecma426 last-modified

--- data.js
+++ data.js
@@ -67,7 +67,7 @@ const linkGroups = [
                 text: "Source map format specification",
                 href: "https://htmlspecs.com/ecma426/",
                 src: "https://tc39.es/ecma426/",
-                "last-modified": "Thu, 24 Sep 2026 04:13:17 GMT",
+                "last-modified": "Thu, 24 Sep 2026 21:36:57 GMT",
                 lang: "cjk",
                 state: "Draft"
             },
